83a56db27c6821e9d677f484c0c1c4c451928cdf
arpit-d <arpitme199@gmail.com>
2020-12-20 21:25:30 +0530

    initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  print(Firebase.apps);
+  await Firebase.initializeApp();
+  print(Firebase.apps);
   runApp(MyApp());
 }
 
@@ -15,6 +20,25 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomeBody(),
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('Avc')),
     );
   }
 }
